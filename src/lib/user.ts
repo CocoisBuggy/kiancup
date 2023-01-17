282098d67e98b0fe5077f1d125bcab4eeb87f15b
CocoisBuggy <g.colinharley@gmail.com>
2023-01-17 19:23:20 +0200

    Added a password compare

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -3,6 +3,12 @@ import { Game } from "./game";
 import { BookingModel, GameModel } from "./models";
 import { UserDoc } from "./types";
 
+import fs from "fs";
+
+const userFile = JSON.parse(
+  fs.readFileSync("./users.json", "utf-8").toString()
+);
+
 export class User {
   doc: UserDoc;
 
@@ -20,5 +26,17 @@ export class User {
     return (await BookingModel.find({ owner: this.doc._id })).map(
       (doc) => new Booking(doc)
     );
+  }
+
+  /**
+   * Check a password against the one on file - these should NEVER be transmitted in the
+   * clear, passwords are to be hashed BEFORE being sent to the server
+   *
+   * code for client:
+   * import sha256 from "crypto-js/sha256";
+   * sha256(password).toString()
+   */
+  passwordCorrect(hashedPassword: string): boolean {
+    return userFile[this.doc.username].password === hashedPassword;
   }
 }
