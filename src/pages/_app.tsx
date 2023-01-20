5a4f7638c2d5bfcc9df448cefd505a98021178fd
Malcolm Redpath <malcolmredpath@gmail.com>
2023-01-20 11:31:40 +0200

    I HATE COMMENTS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-// i wuv comments
+// i DON'T wuv comments
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
